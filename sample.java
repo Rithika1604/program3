b042a4f5305280346880a7c5e5a864e385c62ab7
Rithika1604 <23203021@srcas.ac.in>
2024-07-01 11:58:46 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,11 @@
+class sample{
+  public static void main(String[]args)
+  {
+    System.out.println("Hello");
+    System.out.println("Devops");
+    System.out.println("class");
+    System.out.println("to");
+  }
+}
+    
+  
